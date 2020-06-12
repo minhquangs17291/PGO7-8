dc8da88b168d0f1109ddcbb55cf57dd4346ee86b
MINH QUANG TRAN <61776961+minhquangs17291@users.noreply.github.com>
2020-06-12 23:33:52 +0200

    Add files via upload

---
+++ PGO7/Main.java
@@ -0,0 +1,14 @@
+package PGO7;
+
+public class Main {
+    public static void main(String[] args) {
+        Furniture bed = new Furniture();
+        System.out.println(bed);
+
+        House bedroom = new House();
+        System.out.println(bedroom);
+
+        House.CreateHouse("HongKong1");
+    }
+}
+
